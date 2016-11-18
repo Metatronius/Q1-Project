605171b6b28da034bcf338acbe2845fdcffe0d3e
Paul E Lederer <plederer93@gmail.com>
2016-11-18 09:03:29 -0600

    still not sure why scripts aren't working

--- js/translatize.js
+++ js/translatize.js
@@ -5,14 +5,35 @@
         function(event)
         {
             event.preventDefault();
-            eventDefault();
+            let make = $get(
+                `https://translate.yandex.net/api/v1.5/tr.json/translate ?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=${input}&lang=en-ru>`
+            );
+            for (var i = 0; i < 5; i++)
+            {
+                if (i % 2 === 0)
+                {
+                    make = $get(
+                        "https://translate.yandex.net/api/v1.5/tr.json/translate ?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=" +
+                        make + "&lang=ru-en>"
+                    );
+                    console.log(make);
+                }
+                else
+                {
+                    make = $get(
+                        "https://translate.yandex.net/api/v1.5/tr.json/translate ?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=" +
+                        make + "&lang=en-ru>"
+                    );
+                    console.log(make);
+                }
+            }
+            console.log(make);
         });
 
     $("#poems").click(
 
-        function(event)
+        function()
         {
-            event.preventDefault();
             let poems = $get("http://poetrydb.org/title").titles;
             console.log(poems);
             let random = Math.floor(Math.random() * titles.length);
@@ -21,6 +42,13 @@
                 poems[random] +
                 "/lines.json").lines;
             console.log(poem);
-
+            $("#textarea1").text(poem);
         });
+})
+"http://poetrydb.org/title/" +
+poems[random] +
+    "/lines.json").lines;
+console.log(poem);
+$("#textarea1").text(poem);
+});
 })
