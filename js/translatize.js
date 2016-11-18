a67d840f650a0927b78fe4abd69748d49d2fc395
Paul E Lederer <plederer93@gmail.com>
2016-11-18 12:43:05 -0600

    almost done

--- js/translatize.js
+++ js/translatize.js
@@ -1,54 +1,100 @@
-(function()
-{
-    'use strict';
-    $('#submit').click(
-        function(event)
-        {
-            event.preventDefault();
-            let make = $get(
-                `https://translate.yandex.net/api/v1.5/tr.json/translate ?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=${input}&lang=en-ru>`
-            );
-            for (var i = 0; i < 5; i++)
+'use strict';
+
+
+$('#submit').click(
+    function(event)
+    {
+        let make;
+        event.preventDefault();
+        console.log(
+            `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=${input.value}&lang=en-ja`
+        );
+        $.get(
+                `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=${input.value}&lang=en-ja`
+            )
+            .done(function(data)
             {
-                if (i % 2 === 0)
-                {
-                    make = $get(
-                        "https://translate.yandex.net/api/v1.5/tr.json/translate ?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=" +
-                        make + "&lang=ru-en>"
-                    );
-                    console.log(make);
-                }
-                else
-                {
-                    make = $get(
-                        "https://translate.yandex.net/api/v1.5/tr.json/translate ?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=" +
-                        make + "&lang=en-ru>"
-                    );
-                    console.log(make);
-                }
-            }
-            console.log(make);
-        });
 
-    $("#poems").click(
+                make = data.text[0];
+            })
+            .then(function()
+            {
 
-        function()
-        {
-            let poems = $get("http://poetrydb.org/title").titles;
-            console.log(poems);
-            let random = Math.floor(Math.random() * titles.length);
-            console.log(random);
-            let poem = $get("http://poetrydb.org/title/" +
-                poems[random] +
-                "/lines.json").lines;
-            console.log(poem);
-            $("#textarea1").text(poem);
-        });
-})
-"http://poetrydb.org/title/" +
-poems[random] +
-    "/lines.json").lines;
-console.log(poem);
-$("#textarea1").text(poem);
-});
-})
+                for (var i = 0; i < 5; i++)
+                {
+                    if (i % 2 === 0)
+                    {
+                        $.get(
+                            `http://galvanize-cors-proxy.herokuapp.com/https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=${make}&lang=ja-en`
+                        ).done(function(data)
+                        {
+                            make = data.text[0];
+                        });
+                    }
+                    else
+                    {
+                        $.get(
+                            `http://galvanize-cors-proxy.herokuapp.com/https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=${make}&lang=en-ja`
+                        ).done(function(data)
+                        {
+                            make = data.text[0];
+                        });
+
+                    }
+                }
+
+            }).then(function()
+            {
+                $.get(
+                    `http://galvanize-cors-proxy.herokuapp.com/https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20161115T161210Z.4d649a3b3fe8724e.b7d41665eccf337b03ed8e6e85dda10f353725ec&text=${make}&lang=ja-en`
+                ).done(function(data)
+                {
+                    make = data.text[0];
+                    $("#input").val(make);
+                    $('#input').trigger('autoresize');
+                });
+
+            })
+    });
+$("#poems").click(
+
+    function(event)
+    {
+        let titles = [];
+        let random;
+        event.preventDefault();
+        $.get(
+                "http://galvanize-cors-proxy.herokuapp.com/http://poetrydb.org/title"
+            )
+            .done(function(data)
+            {
+                let poemTitles = data.titles
+                for (var i = 0; i < poemTitles.length; i++)
+                {
+
+                    titles.push(poemTitles[i]);
+                }
+                //console.log(titles);
+                random = Math.floor(Math.random() * titles.length);
+            })
+            .then(function()
+            {
+                $.get(
+                        "http://galvanize-cors-proxy.herokuapp.com/http://poetrydb.org/title/" +
+                        titles[random] +
+                        "/lines.json")
+                    .done(function(data)
+                    {
+                        console.log(data);
+                        console.log(data[0].lines);
+                        let poetry = ''
+                        for (var i = 0; i < data[0].lines.length; i++)
+                        {
+                            poetry += data[0].lines[i] + ' \n';
+                        }
+                        console.log(poetry);
+                        $("#input").val(poetry);
+                        $('#input').trigger('autoresize');
+                    });
+            })
+    });
